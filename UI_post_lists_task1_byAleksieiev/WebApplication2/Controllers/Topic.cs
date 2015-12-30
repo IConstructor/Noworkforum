fad7900238483c73c8b93f481b62ee6a56640c30
Denys Aleksieiev <Denys_Aleksieiev@epam.com>
2015-12-30 10:56:29 +0200

    Third commit.

---
+++ UI_post_lists_task1_byAleksieiev/WebApplication2/Controllers/Topic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication2
+{ 
+    public class Topic
+    {
+        public string Author;
+        public System.DateTime Date;
+        public string Subject;
+        public List<string> Groups;
+        public List<string> Tags;
+    }
+}
